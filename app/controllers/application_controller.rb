d24c350abe984011a3fb4bc0760e3ae782552807
ashzagorski <ash.zagorski@gmail.com>
2018-08-30 21:31:27 -0500

    adds restful routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-  def one_recipe_action
-    @recipe = Recipe.first
-    render 'one_recipe.json.jbuilder'
-  end 
+  protect_from_forgery with: :null_session
 end
